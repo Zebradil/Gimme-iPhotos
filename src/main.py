6761a84b27ea3bb167870db8c4466c0509d7abeb
German Lashevich <german.lashevich@gmail.com>
2020-04-22 01:55:17 +0200

    Refine command help

--- src/main.py
+++ src/main.py
@@ -8,26 +8,58 @@ from downloader import DownloaderApp
 
 def get_cli_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        description="Fetch media from iCloud", argument_default=argparse.SUPPRESS
+        description="Downloads media files from iCloud",
+        argument_default=argparse.SUPPRESS,
     )
 
     # Command line only arguments
     parser.add_argument(
-        "-c", "--config", help="Configuration file", type=argparse.FileType("r")
+        "-c",
+        "--config",
+        help="""
+            Configuration file.
+            It's ini-like file (see configparser module docs), must contain [main] section.
+            Keys are fully-named arguments, except help, config and verbose.
+            Values specified using command line arguments take precedence over values from a provided config file.""",
+        type=argparse.FileType("r"),
     )
     parser.add_argument("-v", "--verbose", action="count", default=0)
 
     # Arguments suitable for configuration file
-    parser.add_argument("-u", "--username")
-    parser.add_argument("-p", "--password")
-    parser.add_argument("-d", "--destination", help="Destination directory")
     parser.add_argument(
-        "-o", "--overwrite", action="store_true", help="Overwrite existing files"
+        "-u",
+        "--username",
+        help="iCloud username (email). Can be specified interactively if not set.",
     )
     parser.add_argument(
-        "-r", "--remove", action="store_true", help="Remove missing files"
+        "-p",
+        "--password",
+        help="iCloud password. Can be specified interactively if not set.",
     )
-    parser.add_argument("-n", "--num-parallel-downloads", dest="parallel", type=int)
+    parser.add_argument(
+        "-d",
+        "--destination",
+        help="Destination directory. Can be specified interactively if not set.",
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite existing files. Default: false.",
+    )
+    parser.add_argument(
+        "-r",
+        "--remove",
+        action="store_true",
+        help="Remove missing files. Default: false.",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-parallel-downloads",
+        dest="parallel",
+        type=int,
+        help="Max number of concurrent downloads. Increase this number if bandwidth is not fully utilized. Default: 3",
+    )
 
     return parser.parse_args()
 
